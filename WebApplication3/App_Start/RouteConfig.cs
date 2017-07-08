7c24a3117b6da01b81650f3c5fa0a372475778b7
苏幸点 <suxingdian@live.com>
2017-07-08 21:30:23 +0800

    添加项目文件。

---
+++ WebApplication3/App_Start/RouteConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebApplication3
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            //此处制定地址的路由格式，此处制定的是：服务器：端口号/helloworld/{controller}/{action}/{id}，以上是路由格式，注意
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Movies", action = "Index", id = UrlParameter.Optional }
+                //此处三个表示默认的启动位置是
+            );
+            
+        }
+    }
+}
